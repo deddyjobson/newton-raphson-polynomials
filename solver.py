d55f804eb644600a6f4377b3435415b53109f263
Deddy Jobson <dedjob@hotmail.com>
2018-08-11 14:28:27 +0530

    minor edits

--- solver.py
+++ solver.py
@@ -4,11 +4,11 @@ import math
 
 from scipy.optimize import newton
 from sklearn.cluster import KMeans
+from time import time
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--default',type=int,default=1)
-parser.add_argument('--display',type=int,default=0)
 parser.add_argument('--save_file',type=str,default='')
 
 params = parser.parse_args()
@@ -101,59 +101,3 @@ for i,r in enumerate(roots):
 print('Displaying with 4 digits of precision.')
 for r in roots:
     print(np.round(r,4))
-
-# to display newton fractals
-if params.display:
-    # Newton fractals
-    # Based on FB - 201003291, http://code.activestate.com/
-    from PIL import Image
-    import matplotlib.pyplot as plt
-
-    imgx = 800
-    imgy = 800
-    image = Image.new("RGB", (imgx, imgy))
-
-    # Complex window
-    xa = -1.0
-    xb = 1.0
-    ya = -1.0
-    yb = 1.0
-
-    maxIt = 20 # max iterations allowed
-    h = 1e-6 # step size for numerical derivative
-    eps = 1e-3 # max error allowed
-
-    # put any complex function here to generate a fractal for it!
-    def f(z):
-        return z * z * z - 1.0
-
-    # draw the fractal
-    print("Running complex newton iterations from %i x %i pixels" % (imgx,imgy), end="... ")
-    for y in range(imgy):
-        zy = y * (yb - ya) / (imgy - 1) + ya
-        for x in range(imgx):
-            zx = x * (xb - xa) / (imgx - 1) + xa
-            z = complex(zx, zy)
-            for i in range(maxIt):
-                # approximation of complex numerical derivative
-                dz = (f(z + complex(h, h)) - f(z)) / complex(h, h)
-                z0 = z - f(z) / dz # Newton iteration
-                if abs(z0 - z) < eps: # stop when close enough to any root
-                    break
-                z = z0
-            r, g, b = i % 4 * 64, i % 8 * 32, i % 16 * 16 # red, green, blue
-            image.putpixel((x, y), (r,g,b))
-    print ("¡bien!")
-
-
-
-
-
-
-
-
-
-
-
-
-pass
