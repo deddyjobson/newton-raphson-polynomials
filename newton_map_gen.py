5ae37b9dea417103e4aad33aed6b4546a42c3d8b
Deddy Jobson <dedjob@hotmail.com>
2018-08-11 20:10:07 +0530

    beautified the map generation

--- newton_map_gen.py
+++ newton_map_gen.py
@@ -4,7 +4,7 @@ import math
 import multiprocessing
 import png
 
-from scipy.optimize import newton
+# from scipy.optimize import newton
 from sklearn.cluster import KMeans
 from time import time
 
@@ -13,6 +13,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--default',type=int,default=1)
 parser.add_argument('--resolution',type=int,default=64)
 parser.add_argument('--extent',type=float,default=2)
+parser.add_argument('--maxiter',type=int,default=1000)
+parser.add_argument('--tol',type=float,default=1e-8)
 parser.add_argument('--save_file',type=str,default='')
 
 params = parser.parse_args()
@@ -47,28 +49,98 @@ z0s = xx+1j*yy
 pprime = np.polyder(p)
 fun = lambda x: np.polyval(p,x)
 
-def fun1(x):
-    print(x)
-    return np.polyval(p,x)
-
 funprime = lambda x: np.polyval(pprime,x)
 
 z0shape = z0s.shape
 z0s = z0s.reshape(-1)
 nmap = np.zeros(z0s.shape, dtype=np.complex_)
 
+def newton(func, x0, fprime=None, args=(), tol=1.48e-8, maxiter=50,
+           fprime2=None):
+    """
+    Find a zero using the Newton-Raphson or secant method.
+    Find a zero of the function `func` given a nearby starting point `x0`.
+    The Newton-Raphson method is used if the derivative `fprime` of `func`
+    is provided, otherwise the secant method is used.  If the second order
+    derivative `fprime2` of `func` is provided, then Halley's method is used.
+    Parameters
+    ----------
+    func : function
+        The function whose zero is wanted. It must be a function of a
+        single variable of the form f(x,a,b,c...), where a,b,c... are extra
+        arguments that can be passed in the `args` parameter.
+    x0 : float
+        An initial estimate of the zero that should be somewhere near the
+        actual zero.
+    fprime : function, optional
+        The derivative of the function when available and convenient. If it
+        is None (default), then the secant method is used.
+    args : tuple, optional
+        Extra arguments to be used in the function call.
+    tol : float, optional
+        The allowable error of the zero value.
+    maxiter : int, optional
+        Maximum number of iterations.
+    fprime2 : function, optional
+        The second order derivative of the function when available and
+        convenient. If it is None (default), then the normal Newton-Raphson
+        or the secant method is used. If it is not None, then Halley's method
+        is used.
+    Returns
+    -------
+    zero : float
+        Estimated location where function is zero.
+    """
+    if tol <= 0:
+        raise ValueError("tol too small (%g <= 0)" % tol)
+    if maxiter < 1:
+        raise ValueError("maxiter must be greater than 0")
+    # Multiply by 1.0 to convert to floating point.  We don't use float(x0)
+    # so it still works if x0 is complex.
+    p0 = 1.0 * x0
+
+
+    # Newton-Rapheson method
+    for iter in range(maxiter):
+        fder = fprime(p0, *args)
+        if fder == 0:
+            msg = "derivative was zero."
+            warnings.warn(msg, RuntimeWarning)
+            return p0
+        fval = func(p0, *args)
+        newton_step = fval / fder
+        if fprime2 is None:
+            # Newton step
+            p = p0 - newton_step
+        else:
+            fder2 = fprime2(p0, *args)
+            # Halley's method
+            p = p0 - newton_step / (1.0 - 0.5 * newton_step * fder2 / fder)
+        if abs(p - p0) < tol:
+            return p, iter # returning number of iterations also
+        p0 = p
+    msg = "Failed to converge after %d iterations, value is %s" % (maxiter, p)
+    raise RuntimeError(msg)
+
+darkness = np.empty_like(nmap,dtype = np.int_)
 for i in range(len(nmap)):
     try:
-        nmap[i] = newton(func=fun, x0=z0s[i], fprime=funprime)
+        nmap[i],darkness[i] = newton(func=fun, x0=z0s[i], fprime=funprime, tol=params.tol, maxiter=params.maxiter)
     except RuntimeError:
-        nmap[i] = 2 # all roots lie within unit circle, so this should work
+        nmap[i],darkness[i] = 2, params.maxiter # all roots lie within unit circle, so this should work
 
 # zeros = list(enumerate(zeros))
 img = np.zeros(shape=(params.resolution,params.resolution,3),dtype=np.uint8)
-colors = [np.random.randint(0,255,size=3,dtype=np.uint8) for _ in zeros]
 
+# colors = [np.random.randint(0,255,size=3,dtype=np.uint8) for _ in zeros]
+colors = []
+while len(colors) < len(zeros):
+    c = np.random.randint(0,255,size=3,dtype=np.uint8)
+    if sum(c) > 400:
+        colors.append(c)
 
 nmap = nmap.reshape(z0shape)
+darkness = darkness.reshape(z0shape)
 
 # creating image
 for i in range(len(nmap[0])):
@@ -76,7 +148,7 @@ for i in range(len(nmap[0])):
         root = nmap[i][j] * upper_bound
         for z,zero in enumerate(zeros):
             if math.isclose(abs(root-zero),0,abs_tol=1e-4):
-                img[i][j] = colors[z]
+                img[i][j] = colors[z] / (1 + math.log10(1+darkness[i][j]))
                 break
 
 
